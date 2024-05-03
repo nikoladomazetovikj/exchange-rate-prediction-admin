f5c2c6a513b9b4f10fe6203c8c26a70c1d27e7c9
nikoladomazetovikj <nikolad150@gmail.com>
2024-05-03 16:16:36 +0200

    fix: last week rates

--- app/Filament/Resources/RateResource/Widgets/RatesChart.php
+++ app/Filament/Resources/RateResource/Widgets/RatesChart.php
@@ -2,24 +2,35 @@
 
 namespace App\Filament\Resources\RateResource\Widgets;
 
+use App\Models\Rate;
 use Filament\Widgets\ChartWidget;
+use Illuminate\Support\Carbon;
 
 class RatesChart extends ChartWidget
 {
-    protected static ?string $heading = 'Rates';
+    protected static ?string $heading = "Last Week Rates";
 
     protected function getData(): array
     {
+        $startOfPreviousWeek = Carbon::now()->subWeek()->startOfWeek();
+        $endOfPreviousWeek = Carbon::now()->subWeek()->endOfWeek();
+
+        $rates = Rate::whereBetween('date', [$startOfPreviousWeek, $endOfPreviousWeek])->get();
+
+        $labels = $rates->map(function ($rate) {
+            return Carbon::parse($rate->date)->format('l');
+        })->toArray();
+
         return [
             'datasets' => [
                 [
                     'label' => 'Real Rates',
-                    'data' => [0, 10, 5, 2, 21, 32, 45, 74, 65, 45, 77, 89],
+                    'data' => $rates->pluck('rate')->toArray(),
                     'backgroundColor' => '#36A2EB',
                     'borderColor' => '#9BD0F5',
                 ],
             ],
-            'labels' => ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+            'labels' => $labels,
         ];
     }
 
